278cf3e82f334a8cbc1e28d9b282898fceb5b1b7
Saravana Chakradhar <saravanachakradhar006@gmail.com>
2026-02-19 15:30:13 +0530

    initial commit - DevSecOps demo

---
+++ src/test/java/com/demo/devsecops_demo/DevsecopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.devsecops_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
